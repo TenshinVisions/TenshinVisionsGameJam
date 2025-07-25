6c65ae9c6401f08695b63b9af5d6a2cde84ef4f3
Marshmallow Siskin <134213041+FinchMarshmallow@users.noreply.github.com>
2025-07-26 00:14:37 +0400

    magick rolle

--- Assets/Main/CODE/UI/Munus/Charecters/CharacterSelectMenu.cs
+++ Assets/Main/CODE/UI/Munus/Charecters/CharacterSelectMenu.cs
@@ -47,9 +47,8 @@ public class CharacterSelectMenu : MonoBehaviour
 		curentSelect = (curentSelect + 1) % mags.Length;
 		trContentPoints.GetChild(trContentPoints.childCount - 1).SetAsFirstSibling();
 
-		// Sorry
-		trContentPoints.GetChild(trContentPoints.childCount - 1).GetComponent<CopyTransformPos>().Obj.position -= Vector3.up * 9999f;
-		trContentPoints.GetChild(0).GetComponent<CopyTransformPos>().Obj.position -= Vector3.up * 9999f;
+		trContentPoints.GetChild(trContentPoints.childCount - 1).GetComponent<CopyTransformPos>().Obj.position += new Vector3(1000, 1000, 0);
+		trContentPoints.GetChild(0).GetComponent<CopyTransformPos>().Obj.position -= new Vector3(1000, 1000, 0);
 
 		UpdateText();
 	}
@@ -59,9 +58,8 @@ public class CharacterSelectMenu : MonoBehaviour
 		curentSelect = (curentSelect - 1) % mags.Length;
 		trContentPoints.GetChild(0).SetSiblingIndex(trContentPoints.childCount - 1);
 
-		// Sorry
-		trContentPoints.GetChild(trContentPoints.childCount - 1).GetComponent<CopyTransformPos>().Obj.position -= Vector3.up * 9999f;
-		trContentPoints.GetChild(0).GetComponent<CopyTransformPos>().Obj.position -= Vector3.up * 9999f;
+		trContentPoints.GetChild(trContentPoints.childCount - 1).GetComponent<CopyTransformPos>().Obj.position -= new Vector3(-1000, 1000, 0);
+		trContentPoints.GetChild(0).GetComponent<CopyTransformPos>().Obj.position += new Vector3(-1000, 1000, 0);
 
 		UpdateText();
 	}

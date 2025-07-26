d2d58ca373802b4076e984291c24408979a192aa
Marshmallow Siskin <134213041+FinchMarshmallow@users.noreply.github.com>
2025-07-26 04:11:01 +0400

    a

--- Assets/Main/CODE/Entity datas systems controllers/Entity.cs
+++ Assets/Main/CODE/Entity datas systems controllers/Entity.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-	public GameObject Datas, Systems;
+	public GameObject Systems;
+
+	[SerializeField] private List<Data> datas;
 
 	private void Awake()
 	{
 		ISystem[] systems = Systems.GetComponents<ISystem>();
-
 
 		foreach (var system in systems)
 		{
 			Debug.Log($"{name}: system: {system}");
 			system.Instal(this);
 		}
+	}
+
+	public bool TryGetData(out Data data)
+	{
+
 	}
 }
